9ce2a26657aede16a045ee58baf873498227577a
Patalano <clement.patalano@gmail.com>
2018-01-12 19:02:37 +0100

    debut tuto

---
+++ portfolio/src/app/projects/projects.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Project } from '../project';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent implements OnInit {
+
+  project = Project {
+    id: 1,
+    name: 'Bloody Foundry'
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

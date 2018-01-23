c07d0facd6a4555976cf7f92488bf7e6cc284f84
Patalano <clement.patalano@gmail.com>
2018-01-23 17:14:18 +0100

    images pour masonry

--- portfolio/src/app/mock-projects.ts
+++ portfolio/src/app/mock-projects.ts
@@ -2,8 +2,8 @@ import { Project } from './project';
 
 export const PROJECTS: Project[]= [
     {id: 11, title: 'Bloody Foundry', image: "assets/images/Bloody Foundry.bmp", importance: 5 },
-    {id: 12, title: 'Immune Defense', image: "assets/images/Bloody Foundry.bmp", importance: 4 },
-    {id: 13, title: 'Cosmopolis', image: "assets/images/Bloody Foundry.bmp", importance: 3 },
-    {id: 14, title: 'Progression Donjon', image: "assets/images/Bloody Foundry.bmp", importance: 4 },
-    {id: 15, title: 'Circuits Trackmania', image: "assets/images/Bloody Foundry.bmp", importance: 2 },
+    {id: 12, title: 'Immune Defense', image: "assets/images/Immune Defense.bmp", importance: 4 },
+    {id: 13, title: 'Cosmopolis', image: "assets/images/Cosmopolis.bmp", importance: 3 },
+    {id: 14, title: 'Progression Donjon', image: "assets/images/Progression Donjon.bmp", importance: 4 },
+    {id: 15, title: 'Circuits Trackmania', image: "assets/images/Circuits Trackmania.bmp", importance: 2 },
 ]
